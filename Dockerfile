17f5c8f3a53cf34443c3932a71ee3054fa03e0bb
philins <philins@ya.ru>
2020-12-09 01:45:20 +0300

    add /log command

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ WORKDIR /home
 
 ENV TELEGRAM_API_TOKEN="1430994393:AAGrMtIDTrF7kwm13niaVIjtBgB0beFTpBI"
 
-RUN pip install -U pip aiogram && apt-get update && apt-get install -y sqlite3
+RUN pip install -U pip aiogram aiofiles && apt-get update && apt-get install -y sqlite3
 COPY *.py ./
 COPY createdb.sql ./
 
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import asyncio
+import aiofiles
 import logging
 import os
 
@@ -10,67 +11,17 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.types import ParseMode
 from aiogram.utils import exceptions, executor
 
+import func
+
+
 API_TOKEN = os.getenv("TELEGRAM_API_TOKEN")
 
-logging.basicConfig(filename="Bot.log", level=logging.INFO)
+logging.basicConfig(filename="bot.log", level=logging.INFO)
 log = logging.getLogger(__name__)
 
 bot = Bot(token=API_TOKEN, parse_mode=types.ParseMode.MARKDOWN)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
-
-
-def get_users():
-    """
-    Return users list
-    In this example returns some random ID's
-    """
-    yield from (114012108, 123456789)
-
-
-async def send_message(user_id: int, text: str, disable_notification: bool = False) -> bool:
-    """
-    Safe messages sender
-    :param user_id:
-    :param text:
-    :param disable_notification:
-    :return:
-    """
-    try:
-        await bot.send_message(user_id, text, disable_notification=disable_notification)
-    except exceptions.BotBlocked:
-        log.error(f"Target [ID:{user_id}]: blocked by user")
-    except exceptions.ChatNotFound:
-        log.error(f"Target [ID:{user_id}]: invalid user ID")
-    except exceptions.RetryAfter as e:
-        log.error(f"Target [ID:{user_id}]: Flood limit is exceeded. Sleep {e.timeout} seconds.")
-        await asyncio.sleep(e.timeout)
-        return await send_message(user_id, text)  # Recursive call
-    except exceptions.UserDeactivated:
-        log.error(f"Target [ID:{user_id}]: user is deactivated")
-    except exceptions.TelegramAPIError:
-        log.exception(f"Target [ID:{user_id}]: failed")
-    else:
-        log.info(f"Target [ID:{user_id}]: success")
-        return True
-    return False
-
-
-async def broadcaster() -> int:
-    """
-    Simple broadcaster
-    :return: Count of messages
-    """
-    count = 0
-    try:
-        for user_id in get_users():
-            if await send_message(user_id, '<b>Hello!</b>'):
-                count += 1
-            await asyncio.sleep(.05)  # 20 messages per second (Limit: 30 messages per second)
-    finally:
-        log.info(f"{count} messages successful sent.")
-
-    return count
 
 
 @dp.message_handler(commands=['start', 'help'])
@@ -85,10 +36,22 @@ async def send_welcome(message: types.Message):
 @dp.message_handler(state='*', commands=['today'])
 async def today_statistics(message: types.Message, state: FSMContext):
     """Отправляет сегодняшнюю статистику трат"""
-    answer_message = await broadcaster()
-    log.info(f"{answer_message} today_statistic.")
+    answer_message = await func.broadcaster()
+    log.info(f"{answer_message} today statistic.")
     async with state.proxy() as proxy:
         await message.answer(f' {proxy}')
+
+@dp.message_handler(state='*', commands=['log'])
+async def view_log(message: types.Message, state: FSMContext):
+    """
+    Отправляет log file
+    """
+    async with aiofiles.open("bot.log", "r") as f:
+        log_text = await f.read()
+        await bot.send_message(
+            message.chat.id,
+            log_text
+        )
 
 
 def check_language(message: types.Message) -> str:
