cf2085a68b7483a481afd80f00a7b9a1f4b98f30
philins <philins@ya.ru>
2020-12-12 03:10:16 +0300

    webhook

--- server.py
+++ server.py
@@ -9,12 +9,22 @@ from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.types import ParseMode
-from aiogram.utils import exceptions, executor
+#from aiogram.utils import exceptions, executor
+from aiogram.utils.executor import start_webhook
 
 from functions import save_new_user, get_total_users, select_companion
 
 
 API_TOKEN = os.getenv("TELEGRAM_API_TOKEN")
+
+# webhook settings
+WEBHOOK_HOST = 'https://62.109.29.107'
+WEBHOOK_PATH = '/'
+WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
+
+# webserver settings
+WEBAPP_HOST = 'localhost'  # or ip
+WEBAPP_PORT = 8443
 
 logging.basicConfig(filename="bot.log", level=logging.INFO)
 log = logging.getLogger(__name__)
@@ -29,7 +39,8 @@ async def send_welcome(message: types.Message):
     """
     This handler will be called when user sends `/start` or `/help` command
     """
-    await message.answer("Hi!\nDo you want some sex, {username}?"
+    await message.answer(
+        "Hi!\nDo you want some sex, {username}?\n Send /new command for registration."
         .format(username=message.from_user.first_name))
 
 
@@ -103,7 +114,9 @@ async def cmd_start(message: types.Message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
     markup.add(message.from_user.first_name)
     markup.add("Cancel")
-    await message.answer("Hi there! What's your name?", reply_markup=markup)
+    await message.answer(
+        "Hi there! What's your name?\nHint: type nickname or select first name",
+        reply_markup=markup)
 
 
 # You can use state '*' if you need to handle all states
@@ -199,6 +212,34 @@ async def process_gender(message: types.Message, state: FSMContext):
     # Finish conversation
     await state.finish()
 
+async def on_startup(dp):
+    await bot.set_webhook(WEBHOOK_URL)
+    # insert code here to run it after start
+
+
+async def on_shutdown(dp):
+    logging.warning('Shutting down..')
+
+    # insert code here to run it before shutdown
+
+    # Remove webhook (not acceptable in some cases)
+    await bot.delete_webhook()
+
+    # Close DB connection (if used)
+    #await dp.storage.close()
+    #await dp.storage.wait_closed()
+
+    logging.warning('Bye!')
+
 
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    #executor.start_polling(dp, skip_updates=True)
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        skip_updates=True,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
